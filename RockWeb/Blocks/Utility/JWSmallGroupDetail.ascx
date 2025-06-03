c89f4be373fde63b57ea6e74b3ee25a365b1d618
jt-wilkinson <realtylerwilkinson@gmail.com>
2025-06-03 12:22:02 -0700

    - Add user control for small group details display.

---
+++ RockWeb/Blocks/Utility/JWSmallGroupDetail.ascx
@@ -0,0 +1,13 @@
+﻿<%@ Control Language="C#" AutoEventWireup="true" CodeFile="JWSmallGroupDetail.ascx.cs" Inherits="RockWeb.Blocks.Utility.JWSmallGroupDetail" %>
+
+<asp:Panel runat="server" ID="pnlContent" CssClass="panel panel-block">
+    <div class="panel-heading">
+        <h1 class="panel-title"><asp:Literal runat="server" ID="ltlName" /></h1>
+    </div>
+    <div class="panel-body">
+        <asp:Literal runat="server" ID="ltlDescription" /><br /><br />
+        <strong>Date Created:</strong> <asp:Literal runat="server" ID="ltlDateCreated" /><br />
+        <strong>Date Modified:</strong> <asp:Literal runat="server" ID="ltlDateModified" /><br />
+        <strong>Capacity:</strong> <asp:Literal runat="server" ID="ltlCapacity" />
+    </div>
+</asp:Panel>
---
+++ RockWeb/Blocks/Utility/JWSmallGroupDetail.ascx.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+using Rock;
+using Rock.Model;
+using Rock.Web.UI;
+using Rock.Web.UI.Controls;
+
+namespace RockWeb.Blocks.Utility
+{
+    public partial class JWSmallGroupDetail : RockBlock
+    {
+        protected override void OnLoad(EventArgs e)
+        {
+            base.OnLoad(e);
+
+            if (!IsPostBack)
+            {
+                ShowGroupDetails();
+            }
+        }
+
+        private void ShowGroupDetails()
+        {
+            int groupId = PageParameter("GroupId").AsInteger();
+            var groupService = new GroupService(new Rock.Data.RockContext());
+            var group = groupService.Get(groupId);
+
+            if (group == null)
+            {
+                ltlName.Text = "Group not found.";
+                pnlContent.Visible = false;
+                return;
+            }
+
+            ltlName.Text = group.Name;
+            ltlDescription.Text = group.Description;
+            ltlDateCreated.Text = group.CreatedDateTime?.ToShortDateString() ?? "N/A";
+            ltlDateModified.Text = group.ModifiedDateTime?.ToShortDateString() ?? "N/A";
+            ltlCapacity.Text = group.GroupCapacity.HasValue ? group.GroupCapacity.Value.ToString() : "Unlimited";
+        }
+    }
+}
